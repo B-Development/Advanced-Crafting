5a4486b62a7aeb29d49f850a162d939f389b0fd8
BlueBeard63 <49408282+BlueBeard63@users.noreply.github.com>
2021-10-15 12:46:32 +0100

    Config redone (testing)

--- AdvancedCrating/Config.cs
+++ AdvancedCrating/Config.cs
@@ -14,7 +14,7 @@ namespace AdvancedCrating
             Recipes = new List<Recipe>()
             {
                 new Recipe("GPS", new Reward(1176, 1) new List<string>{"craft.GPS", "craft.default"}, new Ingredient(66, 3), new Ingredient(1175, 1)),
-                new Recipe("Military Suppressor", 7, new List<string>{"craft.MSuppressor"}, new Ingredient(66, 4), new Ingredient(149, 2))
+                new Recipe("Military Suppressor", new Reward(7, 1), new List<string>{"craft.MSuppressor"}, new Ingredient(66, 4), new Ingredient(149, 2))
             };
         }
     }
